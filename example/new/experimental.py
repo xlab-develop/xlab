b9e93d29b2162b664b8361db3fb02e2edbfd1df6
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2022-02-27 12:09:18 -0500

    Added useful flags and implemented local caching for faster operations on experiments

--- example/new/experimental.py
+++ example/new/experimental.py
@@ -73,19 +73,28 @@ def cache_get_hash(args):
     return hash
 
 def cache_exists(args):
-    hash = cache_get_hash(args)
+    if type(args) == dict:
+        hash = cache_get_hash(args)
+    elif type(args) == str:
+        hash = args
     with open(hashmap_path, 'rb') as in_file:
         cache = pickle.load(in_file)
     return hash in cache
 
-def cache_complete(args):
-    hash = cache_get_hash(args)
+def cache_is_complete(args):
+    if type(args) == dict:
+        hash = cache_get_hash(args)
+    elif type(args) == str:
+        hash = args
     with open(hashmap_path, 'rb') as in_file:
         cache = pickle.load(in_file)
     return hash in cache and cache[hash][1]
 
 def cache_get_dir(args):
-    hash = cache_get_hash(args)
+    if type(args) == dict:
+        hash = cache_get_hash(args)
+    elif type(args) == str:
+        hash = args
     with open(hashmap_path, 'rb') as in_file:
         cache = pickle.load(in_file)
     if hash in cache:
@@ -112,7 +121,10 @@ def cache_assign_dir(args):
     return path
 
 def cache_set_complete(args):
-    hash = cache_get_hash(args)
+    if type(args) == dict:
+        hash = cache_get_hash(args)
+    elif type(args) == str:
+        hash = args
     with open(hashmap_path, 'rb') as in_file:
         cache = pickle.load(in_file)
     cache[hash][1] = True
@@ -134,15 +146,17 @@ class Setup:
     def __init__(self, parser, hash_ignore=[]):
         parser.add_argument("--exp-config", default='{}', type=json.loads)
         parser.add_argument("--exp-dir", default=False, action="store_const", const=True)
+        parser.add_argument("--exp-is-complete", default=False, action="store_const", const=True)
         parser.add_argument("--exp-force", default=False, action="store_const", const=True)
         parser.add_argument("--exp-no-wait", default=False, action="store_const", const=True)
+        parser.add_argument("--exp-hash", default=False, action="store_const", const=True)
 
         self.parser = parser
 
         self._hash_ignore = hash_ignore
     
     def __enter__(self):
-        default_args_keys = ['exp_config', 'exp_dir', 'exp_force', 'exp_no_wait']
+        default_args_keys = ['exp_config', 'exp_dir', 'exp_is_complete', 'exp_force', 'exp_no_wait', 'exp_hash']
         default_config_keys = ['executable']
 
         args = {
@@ -173,13 +187,21 @@ class Setup:
             with open(path, 'w') as out_file:
                 json.dump(config_args, out_file, indent=4)
 
+        if args['exp_hash']:
+            print(cache_get_hash(hash_args))
+            exit(0)
+
         if args['exp_dir']:
-
             print(self.dir)
-
             exit(0)
         
-        if cache_complete(hash_args) and not args['exp_force']:
+        complete = cache_is_complete(hash_args)
+        
+        if args['exp_is_complete']:
+            print(complete)
+            exit(0)
+        
+        if complete and not args['exp_force']:
             print('*** Using cached data on {}'.format(self.dir))
             exit(0)
         
@@ -191,7 +213,7 @@ class Setup:
             traceback.print_exception(exc_type, exc_value, tb)
             return False
 
-        default_args_keys = ['exp_config', 'exp_dir', 'exp_force', 'exp_no_wait']
+        default_args_keys = ['exp_config', 'exp_dir', 'exp_is_complete', 'exp_force', 'exp_no_wait', 'exp_hash']
         default_config_keys = ['executable']
         
         hash_args = substract_dict_keys(self._all_args, 
@@ -211,8 +233,11 @@ class Experiment:
     def __init__(self, executable, req_args, command):
         self.executable = executable
         self.command = command
-        
         self.args = req_args
+        
+        self._last_full_hash = None
+        self._last_local_hash = cache_get_hash(self.args)
+
         dir = self.get_dir()
         
         path = os.path.join(dir, 'config.json')
@@ -223,7 +248,7 @@ class Experiment:
         tmp_args = {
             'executable': self.executable
         }
-        tmp_args = merge_dicts(tmp_args, copy.deepcopy(self.args))
+        tmp_args = merge_dicts(tmp_args, self.args)
 
         command = special_command if special_command != None else self.command
         command = command.format(**tmp_args)
@@ -238,28 +263,48 @@ class Experiment:
         exe.communicate()
         # TODO: catch errors
 
-    def get_dir(self):
+    def get_hash(self):
+        curr_local_hash = cache_get_hash(self.args)
+        if curr_local_hash == self._last_local_hash and self._last_full_hash is not None:
+            return self._last_full_hash
+        self._last_local_hash = curr_local_hash
+
         tmp_args = {
             'executable': self.executable
         }
-        tmp_args = merge_dicts(tmp_args, copy.deepcopy(self.args))
+        tmp_args = merge_dicts(tmp_args, self.args)
 
         command = self.command.format(**tmp_args)
         command_parts = command.split(' ')
-        command_parts.append("--exp-dir")
+        command_parts.append("--exp-hash")
         command_parts += ["--exp-config", "{}".format(json.dumps(self.args))]
 
         exe = Popen(command_parts, stdout=PIPE, stderr=PIPE)
         out, err = exe.communicate()
 
         lines = out.decode(sys.stdin.encoding).split('\n')
+        err_msg = err.decode(sys.stdin.encoding)
+
         if len(lines) < 2:
-            raise Exception("Command didn't return a directory")
+            if len(err) > 0:
+                raise Exception(err_msg)
+            else:
+                raise Exception("error: Command did not print an output.")
 
-        dir = lines[-2]
+        hash = lines[-2]
 
-        if len(err) > 0 and not os.path.exists(dir):
-            error = err.decode(sys.stdin.encoding)
-            raise Exception(error)
+        if not cache_exists(hash):
+            if len(err) > 0:
+                raise Exception(err_msg)
+            else:
+                raise Exception("error: Command returned invalid hash.")
 
-        return dir
+        self._last_full_hash = hash
+
+        return hash
+
+    def get_dir(self):
+        return cache_get_dir(self.get_hash())
+
+    def is_complete(self):
+        return cache_is_complete(self.get_hash())
